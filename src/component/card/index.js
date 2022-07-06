4a2e2e6cac3aa48f3e03fe89b64bfcfb1d9a1ec8
del-alj <douae.elalj@gmail.com>
2022-07-06 20:53:21 +0100

    change data

--- src/component/card/index.js
+++ src/component/card/index.js
@@ -16,10 +16,10 @@ const img =
   "https://i.pinimg.com/564x/70/47/9e/70479e82cd530d08b750df0c08de7773.jpg";
 const data = {
   category: "Shoes",
-  title: "Gabrielle Essence Eau De Parfum",
-  text: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
-  price: 149.99,
-  oldprice: 169.99,
+  title: "SODA Malia Round Toe Stacked Lug Heel Lace Up Ankle Booties",
+  text: "Soda is a fashion shoe brand based in California. Soda offers a wide array of styles including heels, wedges, flats, shoes, sandals and boots,",
+  price: 54.99,
+  oldprice: 99.99,
 };
 
 export const Card = () => {
